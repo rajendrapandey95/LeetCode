2c160c4c16a469bfec6a4ae74e28952657abdad0
Rajendra Pandey <rajendrapandey.95@live.com>
2024-05-27 00:01:10 +0530

    Update 1325. Delete Leaves With a Given Value.py

--- 2024/May/1325. Delete Leaves With a Given Value.py
+++ 2024/May/1325. Delete Leaves With a Given Value.py
@@ -4,7 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# Definition for a binary tree node.
+# Definition for a binary tree node.s
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
