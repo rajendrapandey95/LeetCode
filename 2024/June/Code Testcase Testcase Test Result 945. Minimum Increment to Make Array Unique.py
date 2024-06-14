a53a4eebe44a055feb0a91e56876c32cbb80c120
Rajendra K Pandey <rajendrapandey.95@live.com>
2024-06-14 22:45:48 +0530

    Create  Code Testcase Testcase Test Result 945. Minimum Increment to Make Array Unique.py

---
+++ 2024/June/Code Testcase Testcase Test Result 945. Minimum Increment to Make Array Unique.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        min_increments = 0
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] <= nums[i - 1]:
+                increment = nums[i - 1] + 1 - nums[i]
+                min_increments += increment
+                nums[i] = nums[i - 1] + 1
+        return min_increments
