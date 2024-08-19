1455216238262b666afb01dfcfef285e04739968
Rajendra K Pandey <rajendrapandey.95@live.com>
2024-08-19 19:32:24 +0530

    Create 650. 2 Keys Keyboard.py

---
+++ 2024/August /650. 2 Keys Keyboard.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        ans, d = 0, 2
+        while n > 1:
+            while n % d == 0:
+                ans += d
+                n //= d
+            d += 1
+        return ans
