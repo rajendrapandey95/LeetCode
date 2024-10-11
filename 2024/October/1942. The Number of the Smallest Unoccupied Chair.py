1006b09df0c04e8f0a7d81e5ac0fb13e3b5068f9
Rajendra K Pandey <rajendrapandey.95@live.com>
2024-10-11 21:21:55 +0530

    Update 1942. The Number of the Smallest Unoccupied Chair.py

--- 2024/October/1942. The Number of the Smallest Unoccupied Chair.py
+++ 2024/October/1942. The Number of the Smallest Unoccupied Chair.py
@@ -1,14 +1,15 @@
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
-        target_time = times[targetFriend]
-        times.sort()
-        n = len(times)
-        chair_time = [0] * n
+        events = [[t[0], i] for i, t in enumerate(times)] + [[t[1], ~i] for i, t in enumerate(times)]
+        events.sort()
+        free = list(range(len(times)))
+        occ = []
 
-        for start, end in times:
-            for i in range(n):
-                if chair_time[i] <= start:
-                    chair_time[i] = end
-                    if [start, end] == target_time:
-                        return i
-        return 0
+        for t, f in events:
+            while occ and occ[0][0] <= t:
+                heapq.heappush(free, heapq.heappop(occ)[1])
+            if f >= 0:
+                chair = heapq.heappop(free)
+                if f == targetFriend:
+                    return chair
+                heapq.heappush(occ, [times[f][1], chair])
