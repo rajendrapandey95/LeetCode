42e3e5c58971f7482719a090ef209908e4af68a4
Rajendra K Pandey <rajendrapandey.95@live.com>
2024-10-19 16:07:26 +0530

    Create 1545. Find Kth Bit in Nth Binary String.py

---
+++ 2024/October/1545. Find Kth Bit in Nth Binary String.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findKthBit(self, n: int, k: int) -> str:
+        sequence = "0"
+        
+        for i in range(1, n):
+            if k <= len(sequence):
+                break
+            sequence += "1" + "".join("1" if bit == "0" else "0" for bit in sequence)[::-1]
+
+        return sequence[k - 1]
