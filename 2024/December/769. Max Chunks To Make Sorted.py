f0df8a6f570298aea68ba5577a2bfc0710a2b73e
Rajendra K Pandey <rajendrapandey.95@live.com>
2024-12-19 19:24:51 +0530

    Create 769. Max Chunks To Make Sorted.py

---
+++ 2024/December/769. Max Chunks To Make Sorted.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxChunksToSorted(self, arr):
+        n = len(arr)
+        prefix_max = arr[:]
+        suffix_min = arr[:]
+
+        for i in range(1, n):
+            prefix_max[i] = max(prefix_max[i - 1], prefix_max[i])
+
+        for i in range(n - 2, -1, -1):
+            suffix_min[i] = min(suffix_min[i + 1], suffix_min[i])
+
+        chunks = 0
+        for i in range(n):
+            if i == 0 or suffix_min[i] > prefix_max[i - 1]:
+                chunks += 1
+
+        return chunks
