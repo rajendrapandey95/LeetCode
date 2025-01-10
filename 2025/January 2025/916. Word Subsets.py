80269abde923cee9b4d764c9be61e9596dee1feb
Rajendra K Pandey <rajendrapandey.95@live.com>
2025-01-10 19:37:53 +0530

    Create 916. Word Subsets.py

---
+++ 2025/January 2025/916. Word Subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def wordSubsets(self, A: List[str], B: List[str]) -> List[str]:
+        def count(word):
+            ans = [0] * 26
+            for letter in word:
+                ans[ord(letter) - ord('a')] += 1
+            return ans
+
+        bmax = [0] * 26
+        for b in B:
+            for i, c in enumerate(count(b)):
+                bmax[i] = max(bmax[i], c)
+
+        return [a for a in A if all(x >= y for x, y in zip(count(a), bmax))]
