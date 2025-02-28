84a5662cb804cf16d1999d454b5c4d2a065b2017
Rajendra K Pandey <rajendrapandey.95@live.com>
2025-02-28 19:48:09 +0530

    Create 1092. Shortest Common Supersequence .py

---
+++ 2025/February 2025 /1092. Shortest Common Supersequence .py
@@ -0,0 +1,15 @@
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        m, n = len(str1), len(str2)
+        dp = [[""] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m + 1):
+            for j in range(n + 1):
+                if i == 0 or j == 0:
+                    dp[i][j] = str1[:i] + str2[:j]
+                elif str1[i - 1] == str2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + str1[i - 1]
+                else:
+                    dp[i][j] = min(dp[i - 1][j] + str1[i - 1], dp[i][j - 1] + str2[j - 1], key=len)
+
+        return dp[m][n]
