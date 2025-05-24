2b64f4c32e01f2d8e3ba20cfa1449809ffd00d33
Rajendra K Pandey <rajendrapandey.95@live.com>
2025-05-24 20:32:28 +0530

    Create 2942. Find Words Containing Character.py

---
+++ 2025/May 2025/2942. Find Words Containing Character.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        res = []
+        for i in range(len(words)):
+            if x in words[i]:
+                res.append(i)
+        return res
