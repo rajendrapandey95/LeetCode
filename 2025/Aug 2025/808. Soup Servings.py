f4c74c6109fd128d1fb177e481da64aa2ff750d6
Rajendra K Pandey <rajendrapandey.95@live.com>
2025-08-08 21:22:01 +0530

    Create 808. Soup Servings.py

---
+++ 2025/Aug 2025/808. Soup Servings.py
@@ -0,0 +1,26 @@
+from math import ceil
+
+class Solution:
+    def soupServings(self, n: int) -> float:
+        if n > 4800:
+            return 1  
+        m = ceil(n / 25)
+        dp = {}
+
+        def calculate_dp(i, j):
+            return (dp[max(0, i - 4)][j] +
+                    dp[max(0, i - 3)][max(0, j - 1)] +
+                    dp[max(0, i - 2)][max(0, j - 2)] +
+                    dp[max(0, i - 1)][max(0, j - 3)]) / 4
+
+        dp[0] = {0: 0.5}
+        for k in range(1, m + 1):
+            dp[0][k] = 1
+            dp[k] = {0: 0}
+            for j in range(1, k + 1):
+                dp[j][k] = calculate_dp(j, k)
+                dp[k][j] = calculate_dp(k, j)
+            if dp[k][k] > 1 - 1e-5:
+                return 1
+
+        return dp[m][m]
