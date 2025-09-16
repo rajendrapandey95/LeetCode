72434f9bfa2fd682fa99455f4c0c6a5eee310cff
Rajendra K Pandey <rajendrapandey.95@live.com>
2025-09-16 22:50:53 +0530

    Create 2197. Replace Non-Coprime Numbers in Array.py

---
+++ 2025/Sept 2025/2197. Replace Non-Coprime Numbers in Array.py
@@ -0,0 +1,16 @@
+from typing import List
+from math import gcd
+
+class Solution:
+    def replaceNonCoprimes(self, nums: List[int]) -> List[int]:
+        stack: List[int] = []
+
+        for num in nums:
+            while stack:
+                g = gcd(stack[-1], num)
+                if g == 1:
+                    break
+                num = (stack.pop() * num) // g
+            stack.append(num)
+
+        return stack
