fd2e40e288d62779c38d75994294c978dfda042a
Rajendra K Pandey <rajendrapandey.95@live.com>
2026-01-12 22:08:01 +0530

    Implement minTimeToVisitAllPoints method

---
+++ 2026/Jan 2026/1266. Minimum Time Visiting All Points.py
@@ -0,0 +1,6 @@
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        return sum(
+            max(abs(x2 - x1), abs(y2 - y1))
+            for (x1, y1), (x2, y2) in zip(points, points[1:])
+        )
