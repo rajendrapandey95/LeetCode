3b6e7177faef857aa369d3d3afbb9bfd20a51af3
Rajendra K Pandey <rajendrapandey.95@live.com>
2026-03-08 23:09:57 +0530

    Add solution for finding unique binary string

---
+++ 2026/March 2026/1980. Find Unique Binary String.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        n = len(nums)
+        nums_set = set(nums)
+
+        return "".join("1" if nums[i][i] == "0" else "0" for i in range(n))
