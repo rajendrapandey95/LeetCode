23b10cb33f4074da324517e1eb6ac3983249989b
Rajendra K Pandey <rajendrapandey.95@live.com>
2026-05-14 22:08:52 +0530

    Implement isGood method to check if array is good

---
+++ 2026/May 2026/2784. Check if Array is Good.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isGood(self, nums: List[int]) -> bool:
+        nums.sort()
+        n = len(nums) - 1
+        for i in range(n):
+            if nums[i] != i + 1:
+                return False
+        return nums[n] == n
