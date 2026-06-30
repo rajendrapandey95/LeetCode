81d6ca6c0e4d7a7d34f2b4e28bce91080770c064
Rajendra K Pandey <rajendrapandey.95@live.com>
2026-06-30 21:38:34 +0530

    Create 1358. Number of Substrings Containing All Three Characters.py

---
+++ 2026/June 2026/1358. Number of Substrings Containing All Three Characters.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        left = right = total = 0
+        freq = [0] * 3
+
+        while right < len(s):
+            freq[ord(s[right]) - ord("a")] += 1
+
+            while self._has_all_chars(freq):
+                total += len(s) - right
+
+                freq[ord(s[left]) - ord("a")] -= 1
+                left += 1
+
+            right += 1
+
+        return total
+
+    def _has_all_chars(self, freq: list) -> bool:
+        return all(f > 0 for f in freq)
