c017bbaf2b04e13767fb8d6cdaeff3fbae292c48
Rajendra K Pandey <rajendrapandey.95@live.com>
2026-08-22 20:49:07 +0530

    Update 3622. Check Divisibility by Digit Sum and Product.py

--- 2026/AUG 2026/3622. Check Divisibility by Digit Sum and Product.py
+++ 2026/AUG 2026/3622. Check Divisibility by Digit Sum and Product.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def checkDivisibility(self, n: int) -> bool:
+        n_str=str(n)
+        prod=1
+        su=0
+        for i in n_str:
+            prod*=int(i)
+            su+=int(i)   
+        return n%(prod+su)==0    
