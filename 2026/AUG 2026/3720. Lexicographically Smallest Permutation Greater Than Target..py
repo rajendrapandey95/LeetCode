8a460e108553495a00cc0399d2943b0f9ad1f4b1
Rajendra K Pandey <rajendrapandey.95@live.com>
2026-08-27 22:40:18 +0530

    Create 3720. Lexicographically Smallest Permutation Greater Than Target..py

---
+++ 2026/AUG 2026/3720. Lexicographically Smallest Permutation Greater Than Target..py
@@ -0,0 +1,37 @@
+class Solution:
+    def lexGreaterPermutation(self, s: str, target: str) -> str:
+        cnt = [0] * 26
+
+        for ch in s:
+            cnt[ord(ch) - ord('a')] += 1
+
+        for ch in target:
+            cnt[ord(ch) - ord('a')] -= 1
+
+        for i in range(len(target) - 1, -1, -1):
+            cur = ord(target[i]) - ord('a')
+            cnt[cur] += 1
+
+            if any(x < 0 for x in cnt):
+                continue
+
+            nxt = -1
+            for c in range(cur + 1, 26):
+                if cnt[c]:
+                    nxt = c
+                    break
+
+            if nxt == -1:
+                continue
+
+            cnt[nxt] -= 1
+
+            ans = list(target[:i])
+            ans.append(chr(nxt + ord('a')))
+
+            for c in range(26):
+                ans.extend(chr(c + ord('a')) * cnt[c])
+
+            return ''.join(ans)
+
+        return ""
